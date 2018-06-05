4a6be7a6139c9ed1b17eede4fc171432e512481e
dev <design@dev.com>
2018-06-05 17:09:21 +0530

    new project swift AnimationsSwift

---
+++ Swift-Animations/AppDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  AppDelegate.swift
+//  Swift-Animations
+//
+//  Created by YouXianMing on 16/8/4.
+//  Copyright © 2016年 YouXianMing. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window : UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        /*
+         Lateast no warning version : Xcode 9.3 (9E145)
+         
+         QQ    705786299
+         Email YouXianMing1987@126.com
+         
+         http://www.cnblogs.com/YouXianMing/
+         https://github.com/YouXianMing/YoCelsius
+         https://github.com/YouXianMing/Animations
+         */
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        if let window = window {
+            
+            window.backgroundColor    = UIColor.white
+            let controller            = AnimationsListViewController()
+            let rootViewController    = RootNavigationViewController(rootViewController: controller, hideNavBar: true)
+            window.rootViewController = rootViewController
+            window.makeKeyAndVisible()
+        }
+        
+        return true
+    }
+}
+
