15b2b1a1e7d9e63f958689398cdff639b11028c5
dev <design@dev.com>
2018-06-12 18:40:57 +0530

    bundle id changed

---
+++ DerivedData/Swift-Animations/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-Swift-Animations.build/DerivedSources/Pods_Swift_Animations_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_Swift_AnimationsVersionString[];
+ extern const double Pods_Swift_AnimationsVersionNumber;
+
+ const unsigned char Pods_Swift_AnimationsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_Swift_Animations  PROJECT:Pods-1" "\n";
+ const double Pods_Swift_AnimationsVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ DerivedData/Swift-Animations/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-Swift-Animations.build/Objects-normal/x86_64/Pods_Swift_Animations_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Volumes/cogzidel/Aravind/git/AnimationsSwift/DerivedData/Swift-Animations/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-Swift-Animations.build/DerivedSources/Pods_Swift_Animations_vers.c
